3886b352af0973d79439195f8a9f96935e51d470
Christina Bohn <christinajbohn@gmail.com>
2022-03-17 10:41:51 -0700

    Began file structure for routes.

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,10 @@
+
+const router = require('express').Router();
+
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+const reactionRoutes = require('./reactionRoutes');
+
+
+
+module.exports = router;
